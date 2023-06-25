4f20039bd8a53cd325614235f8fcd704e8c44025
Sakshi Chaurasiya <56647481+sakshichaurasiya123@users.noreply.github.com>
2023-06-25 18:08:39 +0530

    Update AllPossible_SubArray_of_GivenArray.cpp

--- AllPossible_SubArray_of_GivenArray.cpp
+++ AllPossible_SubArray_of_GivenArray.cpp
@@ -18,3 +18,17 @@ for(int i=0; i<4; i++){
 }
 	return 0;
 }
+
+//Using Kadane's algorithm
+    int maxSubArray(vector<int>& nums) {
+        int max_sum = INT_MIN;
+        int cur_sum=0;
+        for(int i=0; i<nums.size(); i++){
+            cur_sum +=nums.at(i);
+            if(cur_sum>max_sum)
+                max_sum=cur_sum;
+            if(cur_sum<0)
+                cur_sum=0;
+        }
+        return max_sum;
+    }
